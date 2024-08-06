bf6ed8558f5297ac1ea8b27f43b34b63c8ebb851
김세영 <ksy2008w@bizpeer.co.kr>
2024-08-06 22:37:00 +0900

    Tip Work

--- app/interiorTip/page.tsx
+++ app/interiorTip/page.tsx
@@ -1,9 +1,19 @@
 export default function InteriorTipPage() {
+    const dummyData = {
+      title: "테스트 타이틀",
+      contents: "<p>테스트 컨텐츠</p><p>테스트</p><p></p>",
+    };
 
     return (
         <>
             <section className="flex min-h-screen flex-col items-center p-10">
-                <p>게시글~~</p>
+                <p className="text-5xl">{dummyData.title}</p>
+              <div className="mt-16">
+                <div dangerouslySetInnerHTML={{__html: dummyData.contents}}></div>
+              </div>
+              <div>
+                <p>여기 다른 팁들 보여주면 됨.</p>
+              </div>
             </section>
         </>
     )
--- components/modals/CreateBoardTipModal.tsx
+++ components/modals/CreateBoardTipModal.tsx
@@ -1,20 +1,125 @@
+import { Input } from "@nextui-org/input";
 import {ModalBody, ModalContent, ModalFooter} from "@nextui-org/modal";
-import StarterKit from '@tiptap/starter-kit'
-import {useEffect} from "react";
-import {EditorContent, useEditor} from "@tiptap/react";
+import dynamic from 'next/dynamic';
+import { useRef, useState } from "react";
+import "react-quill/dist/quill.snow.css";
+import { Button } from "@nextui-org/button";
+import axios from "axios";
+
+const ReactQuill = dynamic(async () => {
+  const {default: RQ} = await import("react-quill");
+  return function comp({forwardedRef, ...props}) {
+    return <RQ ref={forwardedRef} {...props} />;
+  };
+}, {
+  loading: () => <div>...loading</div>,
+  ssr: false,
+});
+
+
 
 export const CreateBoardTipModal = () => {
-    const Editor = useEditor({
-        extensions: [StarterKit],
-        content: '<p>test</p>',
-    })
+    const [content, setContent] = useState<string>();
+    const [title, setTitle] = useState<string>();
+    const [thumbnail, setThumbnail] = useState();
+    const [images, setImages] = useState<any[]>([]);
+    const QuillRef = useRef<ReactQuill>();
+    const thumbnailRef = useRef();
+    const toolbarOptions = [
+      ['bold', 'italic', 'underline', 'strike'],[{ 'align': []}], [{'font': []}], ['link', 'image', 'video', 'formula']
+    ]
+    //let quill =QuillRef.current?.getEditor();
+
+
+    const uploadedThumbnail = () => {
+      //@ts-ignore
+        const file = thumbnailRef.current.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onloadend = (e) => {
+          //@ts-ignore
+          setThumbnail(reader.result);
+        }
+    }
+
+    const onClickSubmit = () => {
+      const contents = {
+        title: title,
+        content: content
+      }
+      const formData = new FormData();
+      formData.append("dto", contents);
+    }
+
+    const imageHandler = async () => {
+      const input = document.createElement("input");
+      input.setAttribute("type", "file");
+      input.setAttribute("accept", "image/*");
+      input.click();
+      input.addEventListener("change", async () => {
+          if(!input.files || !QuillRef.current) return;
+
+          const file = input.files[0];
+
+          const formData = new FormData();
+          formData.append('image', file);
+
+          const range = QuillRef.current?.getEditor().getSelection(true);
+
+          try {
+              const response = await axios.post('/board/image/upload', formData);
+              setImages((prevImagesUrlList)  => [...prevImagesUrlList, response.data]);
+              console.log(response.data);
+              QuillRef.current.getEditor()
+                .insertEmbed(range.index, 'image', response.data);
+          } catch (error) {
+            if(axios.isAxiosError(error)) {
+              if (error.message === 'Network Error') {
+                alert('이미지 크기(10MB)를 초과하였습니다.');
+              }
+            } else {
+              console.error(error);
+            }
+          }
+
+      })
+    }
+
+    if (QuillRef.current) {
+      const toolbar = QuillRef.current.getEditor().getModule('toolbar');
+      toolbar.addHandler('image', imageHandler);
+    }
+
     return(
         <>
             <ModalContent>
                 {(onClose) => (
                     <>
                         <ModalBody>
-                            <EditorContent editor={Editor} />
+                          <h1>인테리어 팁 작성</h1>
+                          <h1>제목</h1>
+                          <Input type="text" size="lg" value={title} onChange={(e) => setTitle(e.target.value)} />
+                          <h1>썸네일 업로드</h1>
+                          {
+                            thumbnail &&
+                            <img className="size-28" src={thumbnail ? thumbnail: ""} alt={"Thumbnail"} />
+                          }
+                          <input type="file" ref={thumbnailRef} accept="image/*" onChange={() => uploadedThumbnail()} />
+                          <h1>내용</h1>
+                          <div className="overflow-y-auto flex justify-center">
+                            <ReactQuill placeholder="인테리어 팁을 작성해 주세요."
+                                        style={{ width: "90%", height: "500px"}}
+                                        forwardedRef={QuillRef}
+                                        theme="snow"
+                                        modules={{
+                                          toolbar: toolbarOptions,
+                                        }}
+                                        value={content}
+                                        onChange={setContent}
+                            />
+                          </div>
+                          <Button onClick={() => onClickSubmit()}>등록하기</Button>
                         </ModalBody>
                         <ModalFooter />
                     </>
